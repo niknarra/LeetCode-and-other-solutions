cf8976282afd0eb9384a29cbd4e88c5277767fd8
Nikhil Narra <nikhilnarra@vt.edu>
2024-10-04 13:02:25 -0400

    problems and code from 10/4

--- Patterns/Patterns.cpp
+++ Patterns/Patterns.cpp
@@ -1,5 +1,276 @@
 // Patterns
 
+/*
+
+1.  *****
+    *****
+    *****
+    *****
+    *****
+
+
+2.  *
+    **
+    ***
+    ****
+    *****
+
+
+3.  *****
+    ****
+    ***
+    **
+    *
+
+
+4.  1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
+
+
+5.  *
+    **
+    ***
+    ****
+    *****
+    ****
+    ***
+    **
+    *
+
+
+6.       *
+        **
+       ***
+      ****
+     *****
+
+
+7.   *****
+      ****
+       ***
+        **
+         *
+
+
+8.      *
+       ***
+      *****
+     *******
+    *********
+
+
+9.  *********
+     *******
+      *****
+       ***
+        *
+
+
+10.      *
+        * *
+       * * *
+      * * * *
+     * * * * *
+
+
+11.  * * * * *
+      * * * *
+       * * *
+        * *
+         *
+
+
+12.  * * * * *
+      * * * *
+       * * *
+        * *
+         *
+         *
+        * *
+       * * *
+      * * * *
+     * * * * *
+
+
+13.      *
+        * *
+       *   *
+      *     *
+     *********
+
+
+14.  *********
+      *     *
+       *   *
+        * *
+         *
+
+
+15.      *
+        * *
+       *   *
+      *     *
+     *       *
+      *     *
+       *   *
+        * *
+         *
+
+
+16.           1
+            1   1
+          1   2   1
+        1   3   3   1
+      1   4   6   4   1
+
+
+17.      1
+        212
+       32123
+      4321234
+       32123
+        212
+         1
+
+
+18.   **********
+      ****  ****
+      ***    ***
+      **      **
+      *        *
+      *        *
+      **      **
+      ***    ***
+      ****  ****
+      **********
+
+
+19.    *        *
+       **      **
+       ***    ***
+       ****  ****
+       **********
+       ****  ****
+       ***    ***
+       **      **
+       *        *
+
+
+20.    ****
+       *  *
+       *  *
+       *  *
+       ****
+
+21.    1
+       2  3
+       4  5  6
+       7  8  9  10
+       11 12 13 14 15
+
+22.    1
+       0 1
+       1 0 1
+       0 1 0 1
+       1 0 1 0 1
+
+23.        *      *
+         *   *  *   *
+       *      *      *
+
+24.    *        *
+       **      **
+       * *    * *
+       *  *  *  *
+       *   **   *
+       *   **   *
+       *  *  *  *
+       * *    * *
+       **      **
+       *        *
+
+25.       *****
+         *   *
+        *   *
+       *   *
+      *****
+
+26.   1 1 1 1 1 1
+      2 2 2 2 2
+      3 3 3 3
+      4 4 4
+      5 5
+      6
+
+27.   1 2 3 4  17 18 19 20
+        5 6 7  14 15 16
+          8 9  12 13
+            10 11
+
+28.      *
+        * *
+       * * *
+      * * * *
+     * * * * *
+      * * * *
+       * * *
+        * *
+         *
+
+29.
+       *        *
+       **      **
+       ***    ***
+       ****  ****
+       **********
+       ****  ****
+       ***    ***
+       **      **
+       *        *
+
+30.         1
+          2 1 2
+        3 2 1 2 3
+      4 3 2 1 2 3 4
+    5 4 3 2 1 2 3 4 5
+
+
+31.      4 4 4 4 4 4 4
+         4 3 3 3 3 3 4
+         4 3 2 2 2 3 4
+         4 3 2 1 2 3 4
+         4 3 2 2 2 3 4
+         4 3 3 3 3 3 4
+         4 4 4 4 4 4 4
+
+32.    E
+       D E
+       C D E
+       B C D E
+       A B C D E
+
+33.    a
+       B c
+       D e F
+       g H i J
+       k L m N o
+
+34.    E D C B A
+       D C B A
+       C B A
+       B A
+       A
+
+35.    1      1
+       12    21
+       123  321
+       12344321
+*/
+
+
 #include<iostream>
 using namespace std;
 
@@ -186,14 +457,225 @@ void pattern11(int size){ // Reversed Equilateral Triangle with spacing in betwe
 
 }
 
+void pattern12(int size){ // Reversed Equilateral Triangle with spacing in between stars + Equilateral Triangle with spacing in between stars
+
+    pattern11(size);
+
+    pattern10(size);
+
+}
+
+// void pattern13(int size){
+
+//     for(int i=0;i<size;i+=1){
+
+//         for(int j)
+
+//     }
+
+// }
+
+void pattern18(int size){
+
+    // Upper Half
+
+    for(int i=0;i<size;i+=1){
+
+        for(int j=size;j>i;j-=1){
+            cout<<"*";
+        }
+
+        for(int k=0;k<(2*i);k+=1){
+            cout<<" ";
+        }
+
+        for(int l=size;l>i;l-=1){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    // Lower Half
+
+    for(int i=0;i<size;i+=1){
+
+        for(int j=0;j<=i;j+=1){
+            cout<<"*";
+        }
+
+        for(int k=1;k<(2*size)-(2*i)-1;k+=1){
+            cout<<" ";
+        }
+
+        for(int l=0;l<=i;l+=1){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern19(int size){
+
+    for(int i=1;i<=size;i+=1){
+
+        for(int j=1;j<=i;j+=1){
+            cout<<"*";
+        }
+
+        for(int k=0;k<(2*size)-(2*i);k+=1){
+            cout<<" ";
+        }
+
+        for(int l=i;l>0;l-=1){
+            cout<<"*";
+        }
+
+        cout<<endl;
+    }
+
+    for (int i = size - 1; i >= 1; i -= 1) {
+        for (int j = 1; j <= i; j += 1) {
+            cout << "*";
+        }
+
+        for (int k = 0; k < (2 * size) - (2 * i); k += 1) {
+            cout << " ";
+        }
+
+        for (int l = i; l > 0; l -= 1) {
+            cout << "*";
+        }
+
+        cout<<endl;
+    }
+}
+
+void pattern20(int size){
+
+    for(int i=0;i<size;i+=1){
+
+        if(i == 0 || i==size-1){
+            for(int j = 0;j<size;j+=1){
+                cout<<"*";
+            }
+        }
+        else{
+            cout<<"*";
+
+            for(int l=0;l<size-2;l+=1){
+                cout<<"-";
+            }
+
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern21(int size){
+
+    for(int i=1;i<=size;i+=1){
+
+        for(int j=i+(i-1);j<=size;j+=1){
+
+            cout<<i;
+
+        }
+        cout<<endl;
+    }
+}
+
+void pattern26(int size){
+
+    for(int i=1;i<=size;i+=1){
+
+        for(int j=i;j<=size;j+=1){
+
+            cout<<i;
+
+        }
+        cout<<endl;
+    }
+}
+
+void pattern28(int size){
+    // Upper Half
+    for(int i=0;i<size;i+=1){
+
+        for(int j=i;j<size-1;j+=1){
+            cout<<' ';
+        }
+
+        for(int k=0;k<=i;k+=1){
+            cout<<"* ";
+        }
+
+        cout<<endl;
+    }
+    // Lower Half
+    for(int i=0;i<size-1;i+=1){
+
+        for(int j=0;j<i+1;j+=1){
+            cout<<' ';
+        }
+
+        for(int k=size-1;k>i;k-=1){
+            cout<<"* ";
+        }
+
+        cout<<endl;
+    }
+}
+
+void pattern32(char size){
+
+    for(char i=size;i>='A';i-=1){
+
+        for(char j=i;j<=size;j+=1){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern34(char size){
+
+    for(char i=size;i>='A';i-=1){
+
+        for(char j=i;j>='A';j-=1){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern35(int size){
+
+    for(int i=1;i<=size;i+=1){
+
+        for(int j=1;j<=i;j+=1){
+            cout<<j;
+        }
+
+        for(int k=0;k<(2*size)-(2*i);k+=1){
+            cout<<" ";
+        }
+
+        for(int l=i;l>0;l-=1){
+            cout<<l;
+        }
+
+        cout<<endl;
+    }
+}
+
 int main(){
 
 	cout<<"Welcome to Patterns"<<endl;
 
-	pattern11(5);
+	pattern28(5);
 
 	return 0;
 
 
 }
-
